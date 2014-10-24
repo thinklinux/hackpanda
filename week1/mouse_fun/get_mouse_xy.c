01de2de55991c35435826a2f03bd2d5e45026063
Tihomir Valkanov <tihomir.valkanov@gmail.com>
2014-10-24 20:03:29 +0300

    mouse fun task - move the mouse 100px right for 5 times

---
+++ week1/mouse_fun/get_mouse_xy.c
@@ -0,0 +1,44 @@
+// cat /proc/bus/input/devices | find what eventN you need for your mouse
+
+#include <stdio.h>
+#include <string.h>
+#include <linux/input.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <stddef.h>
+#include <unistd.h>
+
+int main() {
+  struct input_event event, event_end;
+
+  int fd = open("/dev/input/event16", O_RDWR);
+
+  if (fd < 0) {
+    printf("Errro open mouse:%s\n", strerror(errno));
+    return -1;
+  }
+
+  memset(&event, 0, sizeof(event));
+  memset(&event, 0, sizeof(event_end));
+  gettimeofday(&event.time, NULL);
+  event.type = EV_REL;
+  event.code = REL_X;
+  event.value = 100;
+
+  gettimeofday(&event_end.time, NULL);
+  event_end.type = EV_SYN;
+  event_end.code = SYN_REPORT;
+  event_end.value = 0;
+
+  for (int i=0; i<5; i++) {
+    write(fd, &event, sizeof(event)); // Move the mouse
+    write(fd, &event_end, sizeof(event_end)); // Show move
+    sleep(1); // Wait
+  }
+
+  close(fd);
+  return 0;
+}
